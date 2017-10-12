376dc25902b0316223a046af8258a6cd09b3a0d9
Vistor25 <victorvoronko@gmail.com>
2017-10-13 01:20:15 +0300

    Add project files.

---
+++ ClassLibrary1/FileSystemVisitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSystem
+{
+    public class FileSystemVisitor
+    {
+        public List<string> dirs = new List<string>();
+
+        public void GetDerictories(string directiryName)
+        {
+            string[] directories = new string[] { };
+            string[] files = new string[] { };
+
+
+            try
+            {
+                directories = Directory.GetDirectories(directiryName);
+                files = Directory.GetFiles(directiryName);
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                
+            }
+
+            dirs.AddRange(files);
+
+            foreach (var directiry in directories)
+            {
+                
+                dirs.Add(directiry);
+                
+                GetDerictories(directiry);
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirname = Console.ReadLine();
+            FileSystemVisitor fl = new FileSystemVisitor();
+            fl.GetDerictories(dirname);
+            
+            foreach(var name in fl.dirs)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
